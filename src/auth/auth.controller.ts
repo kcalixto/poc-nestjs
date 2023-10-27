e87f0292ac77d33ec2b34d49b071decc00f7e15c
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-10-27 02:46:46 -0300

    added initial controllers & services

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post('login')
+    async login() {
+        return this.authService.login();
+    }
+
+    @Post('signup')
+    async signup() {
+        return this.authService.singup();
+    }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService {
+    constructor() { }
+
+    login() {
+        return "test"
+    }
+
+    singup() {
+
+    }
+}
