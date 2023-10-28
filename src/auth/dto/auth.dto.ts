7c211bca60c0382de2cdd9d34d6ae7b7f9b1f4b2
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-10-28 16:15:35 -0300

    added auto swagger

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,10 +1,12 @@
-import { IsEmail, IsNotEmpty, IsString } from "class-validator"
+import { IsEmail, IsNotEmpty, IsString, Max } from "class-validator"
 
 export class AuthDTO {
+    @Max(255)
     @IsEmail()
     @IsNotEmpty()
     username: string;
 
+    @Max(64)
     @IsString()
     @IsNotEmpty()
     password: string;
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, // remove all the properties that are not in the DTO
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Poc NestJS')
+    .setDescription('Nestjs proof of concept')
+    .setVersion('0.1')
+    .addTag('auth')
+    .addTag('user')
+    .addTag('bookmark')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
